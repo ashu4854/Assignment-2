24a034ed28208c6d702eb01e996dca9c04536735
Ashutosh Sharma <as211@utas.edu.au>
2026-09-15 02:34:19 +1000

    Fixed Cluster constructor syntax

--- Provisions - Array Collection/Cluster.java
+++ Provisions - Array Collection/Cluster.java
@@ -4,7 +4,7 @@
  * KIT107 Assignment 2 -- Cluster Implementation
  * 
  * @author <<Ashutosh Sharma 777197>>
- * @version	<<15/09/2026>>
+ * @version <<15/09/2026>>
  */
 
 public class Cluster implements ClusterInterface
@@ -13,245 +13,239 @@ public class Cluster implements ClusterInterface
     protected String bundleName;        // candidate for whom this cluster of votes is for
     protected double weightedCount;     // weight of votes in this cluster
     protected int rawCount;             // raw count of votes in this cluster
-	protected Ballot []ballots;
+    protected Ballot []ballots;
 
-	/**
-	 * Constructor
-	 * 
+    /**
+     * Constructor
+     * 
      * @param candidate String -- the name of the candidate whose votes 
      *                  this bundle (cluster) is for
      * 
-	 * Precondition: The String is defined and unique
-	 * Postcondition: The new instance will have its instance variable(s)
+     * Precondition: The String is defined and unique
+     * Postcondition: The new instance will have its instance variable(s)
      *                  initialised to indicate an empty cluster.
-	 * Informally: Initialise the cluster of ballots.
-	 */
+     * Informally: Initialise the cluster of ballots.
+     */
     public Cluster(String candidate)
     {
-      public Cluster(String candidate)
-{
-    bundleName = candidate;
-    weightedCount = 0.0;
-    rawCount = 0;
-    ballots = new Ballot[0];
-}
+        bundleName = candidate;
+        weightedCount = 0.0;
+        rawCount = 0;
+        ballots = new Ballot[0];
     }
 
-	/**
-	 * isEmpty()
-	 * 
-	 * @return boolean -- whether the cluster is empty
-	 * 
-	 * Precondition: None
-	 * Postcondition: True is returned if the Cluster is empty; false is
+    /**
+     * isEmpty()
+     * 
+     * @return boolean -- whether the cluster is empty
+     * 
+     * Precondition: None
+     * Postcondition: True is returned if the Cluster is empty; false is
      *                  returned otherwise.
-	 * Informally: Check whether the Cluster is empty.
-	 */
+     * Informally: Check whether the Cluster is empty.
+     */
     public boolean isEmpty()
     {
-        return rawCount == 0;  
+        return rawCount == 0;
     }
 
- 	/**
-	 * getFirstBallot()
-	 * 
-	 * @return Ballot -- the first ballot paper in the cluster
-	 * 
-	 * Precondition: None
-	 * Postcondition: the first ballot in the cluster is returned if the
+    /**
+     * getFirstBallot()
+     * 
+     * @return Ballot -- the first ballot paper in the cluster
+     * 
+     * Precondition: None
+     * Postcondition: the first ballot in the cluster is returned if the
      *                  cluster is non-empty; null is returned otherwise.
-	 * Informally: Get the first ballot paper in the cluster.
-	 */
+     * Informally: Get the first ballot paper in the cluster.
+     */
     public Ballot getFirstBallot()
     {
         if (rawCount == 0)
-    {
-        return null;
+        {
+            return null;
+        }
+
+        return ballots[0];
     }
 
-    return ballots[0]; 
-
-    }
-
- 	/**
-	 * getRawCount()
-	 * 
-	 * @return int -- the raw count of ballot papers in the cluster
-	 * 
-	 * Precondition: None
-	 * Postcondition: the raw count of ballots in the cluster is returned.
-	 * Informally: Get the count of ballots in the cluster.
-	 */
+    /**
+     * getRawCount()
+     * 
+     * @return int -- the raw count of ballot papers in the cluster
+     * 
+     * Precondition: None
+     * Postcondition: the raw count of ballots in the cluster is returned.
+     * Informally: Get the count of ballots in the cluster.
+     */
     public int getRawCount()
     {
         return rawCount;
     }
 
- 	/**
-	 * getWeightedCount()
-	 * 
-	 * @return double -- the weighted count of ballot papers in the cluster
-	 * 
-	 * Precondition: None
-	 * Postcondition: the weighted count of ballots in the cluster is
+    /**
+     * getWeightedCount()
+     * 
+     * @return double -- the weighted count of ballot papers in the cluster
+     * 
+     * Precondition: None
+     * Postcondition: the weighted count of ballots in the cluster is
      *                  returned.
-	 * Informally: Get the weighted count of ballots in the cluster.
-	 */
+     * Informally: Get the weighted count of ballot papers in the cluster.
+     */
     public double getWeightedCount()
     {
         return weightedCount;
     }
 
- 	/**
-	 * getBundleName()
-	 * 
-	 * @return String -- the name of the candidate that is the recipient
+    /**
+     * getBundleName()
+     * 
+     * @return String -- the name of the candidate that is the recipient
      *                  of this cluster of ballots
-	 * 
-	 * Precondition: None
-	 * Postcondition: the name of the bundle is returned.
-	 * Informally: Get the cluster's candidate name.
-	 */
+     * 
+     * Precondition: None
+     * Postcondition: the name of the bundle is returned.
+     * Informally: Get the cluster's candidate name.
+     */
     public String getBundleName()
     {
         return bundleName;
     }
 
     /**
-	 * addBallotToCluster()
-	 * 
-	 * @param votes Ballot -- the ballot paper to add to this cluster
-	 * 
-	 * Precondition: The given Ballot parameter has been constructed.
-	 * Postcondition: The given Ballot has been added to the Cluster of
+     * addBallotToCluster()
+     * 
+     * @param votes Ballot -- the ballot paper to add to this cluster
+     * 
+     * Precondition: The given Ballot parameter has been constructed.
+     * Postcondition: The given Ballot has been added to the Cluster of
      *                  ballot papers ordered by descending preference.
-	 * Informally: Add a ballot paper to the Cluster.
-	 */
+     * Informally: Add a ballot paper to the Cluster.
+     */
     public void addBallotToCluster(Ballot votes)
     {
-      if (rawCount == this.ballots.length)
-{
-    Ballot []newBallots = new Ballot[rawCount + 1];
+        if (rawCount == this.ballots.length)
+        {
+            Ballot []newBallots = new Ballot[rawCount + 1];
 
-    for (int i = 0; i < rawCount; i++)
-    {
-        newBallots[i] = this.ballots[i];
+            for (int i = 0; i < rawCount; i++)
+            {
+                newBallots[i] = this.ballots[i];
+            }
+
+            this.ballots = newBallots;
+        }
+
+        int position = rawCount;
+
+        while (position > 0 &&
+               this.ballots[position - 1].getChoice() > votes.getChoice())
+        {
+            this.ballots[position] = this.ballots[position - 1];
+            position--;
+        }
+
+        this.ballots[position] = votes;
+
+        rawCount++;
+        weightedCount += votes.getWeight();
     }
 
-    this.ballots = newBallots;
-}
-
-int position = rawCount;
-
-while (position > 0 &&
-       this.ballots[position - 1].getChoice() > votes.getChoice())
-{
-    this.ballots[position] = this.ballots[position - 1];
-    position--;
-}
-
-this.ballots[position] = votes;
-
-rawCount++;
-weightedCount += votes.getWeight();
-    }
-
- 	/**
-	 * votesFor()
-	 * 
-	 * @param candidate String -- the candidate to count the votes of
+    /**
+     * votesFor()
+     * 
+     * @param candidate String -- the candidate to count the votes of
      * @param preference int -- the preference to count the votes for
      * 
      * @return int -- the count of votes for the given candidate of the
      *                  given preference
-	 * 
-	 * Precondition: None
-	 * Postcondition: the first ballot in the cluster is returned if the
-     *                  cluster is non-empty; null is returned otherwise.
-	 * Informally: Get the first ballot paper in the cluster.
-	 */
+     * 
+     * Precondition: None
+     * Postcondition: the count of votes for the given candidate at the
+     *                  given preference is returned.
+     * Informally: Count votes for a candidate at a given preference.
+     */
     public int votesFor(String candidate, int preference)
     {
-      int count = 0;
+        int count = 0;
 
-for (int i = 0; i < rawCount; i++)
-{
-    if (preference < ballots[i].getMaxVote() &&
-        ballots[i].getVotes()[preference].equalsIgnoreCase(candidate))
-    {
-        count++;
+        for (int i = 0; i < rawCount; i++)
+        {
+            if (preference < ballots[i].getMaxVote() &&
+                ballots[i].getVotes()[preference].equalsIgnoreCase(candidate))
+            {
+                count++;
+            }
+        }
+
+        return count;
     }
-}
 
-return count;
-
-    }
-
- 	/**
-	 * transfer()
-	 * 
+    /**
+     * transfer()
+     * 
      * @param residiual double -- the residual weight to be allocated to
      *                  the ballot being transferred
      * 
-	 * @return Ballot -- the ballot removed from the current cluster
+     * @return Ballot -- the ballot removed from the current cluster
      *                  which is to be moved to another cluster with the
      *                  given weight
-	 * 
-	 * Precondition: None
-	 * Postcondition: the first ballot in the cluster is removed, the
+     * 
+     * Precondition: None
+     * Postcondition: the first ballot in the cluster is removed, the
      *                  selection is updated to the next preference,
      *                  the weight is altered if the residual is not
      *                  full weight, and then the ballot is returned.
      *                  null is returned if the cluster is empty.
-	 * Informally: Prepare the first ballot of the cluster to be
+     * Informally: Prepare the first ballot of the cluster to be
      *                  moved to the cluster of its next preference,
      *                  and remove it from this cluster.
-	 */
+     */
     public Ballot transfer(double residual)
     {
-      Ballot result;
+        Ballot result;
 
-if (rawCount == 0)
-{
-    return null;
-}
+        if (rawCount == 0)
+        {
+            return null;
+        }
 
-result = ballots[0];
+        result = ballots[0];
 
-for (int i = 1; i < rawCount; i++)
-{
-    ballots[i - 1] = ballots[i];
-}
+        for (int i = 1; i < rawCount; i++)
+        {
+            ballots[i - 1] = ballots[i];
+        }
 
-rawCount--;
-weightedCount -= result.getWeight();
+        rawCount--;
+        weightedCount -= result.getWeight();
 
-result.update();
+        result.update();
 
-if (residual != -1)
-{
-    result.setWeight(residual);
-}
+        if (residual != -1)
+        {
+            result.setWeight(residual);
+        }
 
-return result;
-       
+        return result;
     }
 
-	/**
-	 * toString()
-	 * 
-	 * @return String -- printable form of the Cluster of ballots
-	 * 
-	 * Precondition: None
-	 * Postcondition: A printable (String) form of the ballot data is
+    /**
+     * toString()
+     * 
+     * @return String -- printable form of the Cluster of ballots
+     * 
+     * Precondition: None
+     * Postcondition: A printable (String) form of the ballot data is
      *                  returned.  If there are no ballot papers then ""
      *                  is returned.
-	 * Informally: Convert the Cluster of ballot data to a multi-line
+     * Informally: Convert the Cluster of ballot data to a multi-line
      *                  String.
-	 */
+     */
     public String toString()
     {
-//COMPLETE ME!!!
+        //COMPLETE ME!!!
         return "UNFINISHED";  // change me -- this is just to allow the program to compile
     }
 }
